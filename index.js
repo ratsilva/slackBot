9b3c5ffdcc35e3de9553c8b7ed48206a66defb01
Ricardo Silva <ricardoatsilva1@gmail.com>
2019-11-10 17:58:53 -0300

    updating index.js

--- index.js
+++ index.js
@@ -7,3 +7,50 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.PORT ||
 } else {
   console.log('Good job, you have the variables!')
 }
+
+var controller = Botkit.slackbot({
+ json_file_store: './db_slackbutton_slash_command/',
+ debug: true,
+ clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+})
+
+controller.configureSlackApp({
+ clientId: process.env.CLIENT_ID,
+ clientSecret: process.env.CLIENT_SECRET,
+ clientSigningSecret: process.env.CLIENT_SIGNING_SECRET,
+ scopes: ['commands', 'bot'],
+})
+
+var bot = controller.spawn({
+  token: process.env.BOT_TOKEN,
+  incoming_webhook: {
+    url: 'WE_WILL_GET_TO_THIS'
+  }
+}).startRTM();
+
+controller.setupWebserver(process.env.PORT, function(err, webserver){
+ controller.createWebhookEndpoints(controller.webserver);
+ controller.createOauthEndpoints(controller.webserver, 
+   function(err, req, res) {
+     if (err) {
+       res.status(500).send('ERROR: ' + err);
+     } else {
+       res.send('Success!');
+     }
+   });
+});
+
+controller.hears('hi', 'direct_message', function(bot, message) {
+ bot.reply(message,'Hello!');
+});
+
+controller.on('slash_command', function(bot, message) {
+ bot.replyAcknowledge()
+ switch (message.command) {
+   case '/echo':
+   bot.reply(message, 'heard ya!')
+   break;
+ default: 
+   bot.reply(message, 'Did not recognize that command, sorry!')
+ }
+});
